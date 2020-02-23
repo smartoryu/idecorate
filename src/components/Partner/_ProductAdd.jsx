ad2eb55b0358b1d757ccc9ff8fb88ac24fe18e8e
smartoryu <prikenang@gmail.com>
2020-02-23 21:33:12 +0700

    add form validation and early connection to backend

--- src/components/Partner/_ProductAdd.jsx
+++ src/components/Partner/_ProductAdd.jsx
@@ -1,51 +1,52 @@
 /* eslint-disable no-unused-vars */
-import React, { Fragment, useState, useCallback, useReducer } from "react";
-// import { useDropzone } from "react-dropzone";
+import React, { Fragment, useState, useReducer } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   Col,
   CustomInput,
   Form,
+  FormFeedback,
   FormGroup,
   Label,
   Input,
   InputGroup,
   InputGroupAddon,
-  Card,
-  CardImg,
-  CardBody,
-  CardText,
-  CardFooter,
-  Button,
   Tooltip
 } from "reactstrap";
 import { MdAdd } from "react-icons/md";
-import Dropzone from "react-dropzone-uploader";
-
-import "react-dropzone-uploader/dist/styles.css";
+import { Redirect } from "react-router-dom";
+import Axios from "axios";
 import { API_URL } from "../../support/API_URL";
 
 export function AddProduct() {
-  // const [addImage, setAddImage] = useState([]);
-  const [product, setProduct] = useState({});
-  const [tooltipOpen, setTooltipOpen] = useState(false);
-  const toggleTooltip = () => setTooltipOpen(!tooltipOpen);
+  /**
+   * ============================================= REDUX REDUCER =====
+   */
+  const dispatch = useDispatch();
+  const isRedirect = useSelector(state => state.product.redirect);
+  const ErrorName = useSelector(state => state.product.errorName);
 
-  const [addImage, dispatch] = useReducer((addImage, { type, value }) => {
+  /**
+   * ===================================================== STATE =====
+   */
+  const [product, setProduct] = useState({});
+  const [tooltipOpen, setTooltipOpen] = useState("");
+  const toggleTooltip = id => setTooltipOpen(id);
+
+  const [addImage, setImage] = useReducer((addImage, { type, value }) => {
     switch (type) {
       case "add":
-        return [...addImage, value];
+        return addImage.length < 4 ? [...addImage, value] : addImage;
       case "remove":
         return addImage.filter((_, index) => index !== value);
       default:
-        return addImage;
+        return addImage.filter(val => val);
     }
   }, []);
 
   const handleAddImage = acceptedFiles => {
-    // console.log(acceptedFiles);
-
     acceptedFiles.forEach(File => {
-      dispatch({ type: "add", value: { FileName: File.name, File } });
+      setImage({ type: "add", value: { FileName: File.name, File } });
     });
   };
 
@@ -64,9 +65,28 @@ export function AddProduct() {
     formData.append("image", addImage.File);
     formData.append("data", JSON.stringify(data));
     console.log(formData);
+
+    if (data.name) {
+      try {
+        let { data } = Axios.post(`${API_URL}/product/add`, formData, Headers);
+
+        if (data.redirect) {
+          dispatch({ type: "ADD_PRODUCT_SUCCESS" });
+        }
+      } catch (err) {
+        console.log(err);
+        dispatch({ type: "SERVER_ERROR", payload: "Server error!" });
+      }
+    } else {
+      dispatch({ type: "EMPTY_PRODUCT_NAME", payload: "Enter product name" });
+    }
   };
 
   console.log(addImage);
+
+  if (isRedirect) {
+    return <Redirect to="/partner/product" />;
+  }
 
   return (
     <Fragment>
@@ -78,11 +98,13 @@ export function AddProduct() {
           <Col sm={10}>
             <Input
               onChange={e => setProduct({ ...product, name: e.target.value })}
+              invalid={ErrorName}
               type="text"
               name="product_name"
               id="product_name"
               placeholder="input product name"
             />
+            {ErrorName ? <FormFeedback className="float-left">{ErrorName}</FormFeedback> : null}
           </Col>
         </FormGroup>
 
@@ -163,70 +185,46 @@ export function AddProduct() {
               onChange={({ target }) => handleAddImage(Array.from(target.files))}
               id="product_image"
               className="add_product_input"
-              // multiple
+              multiple
               max={4}
               accept="image/png, image/jpeg"
               type="file"
             />
-            {/* <Label sm={12}>{AddImageDropzone()}</Label> */}
 
             {addImage.length > 0 &&
-              addImage.map((image, id) => {
-                return (
-                  <Label sm={2} key={id} id={"img-" + id}>
-                    <div className="add_product_preview">
+              addImage.map((image, id) =>
+                id <= 3 ? (
+                  <Label
+                    sm={2}
+                    key={"label" + id}
+                    id={"image-" + id}
+                    onMouseEnter={() => toggleTooltip(id)}
+                    onMouseLeave={() => toggleTooltip(-1)}>
+                    <div key={"div" + id} className="add_product_preview">
                       <img
-                        onClick={() => dispatch({ type: "remove", value: id })}
+                        key={"img" + id}
+                        onClick={() => setImage({ type: "remove", value: id })}
                         src={URL.createObjectURL(image.File)}
                         className="plus_icon overflow-hidden"
                         alt=""
                       />
                     </div>
-                    <div className="remove_icon text-center">
-                      <Tooltip placement="bottom" isOpen={tooltipOpen} target={"img-" + id} toggle={toggleTooltip}>
-                        Click to remove!
+
+                    {id < 0 ? (
+                      <Tooltip
+                        key={"tooltip" + id}
+                        placement="bottom"
+                        isOpen={id === tooltipOpen}
+                        target={"image-" + id}
+                        toggle={() => toggleTooltip(id)}>
+                        {image.FileName} <br />
+                        {`image-${id}`} <br />
+                        <b>Click to remove!</b>
                       </Tooltip>
-                    </div>
+                    ) : null}
                   </Label>
-
-                  // <Label sm={3} key={id}>
-                  //   <Card className="add_product_card">
-                  //     <span
-                  //       onClick={() => dispatch({ type: "remove", value: id })}
-                  //       className="float-right"
-                  //       style={{ cursor: "pointer" }}>
-                  //       x
-                  //     </span>
-                  //     <CardImg top width="100%" src={URL.createObjectURL(image.File)} alt="product" />
-                  //     <CardBody>
-                  //       <CardText onClick={() => dispatch({ type: "remove", value: id })} style={{ cursor: "pointer" }}>
-                  //         {image.FileName}
-                  //       </CardText>
-                  //       <Button className="float-right" onClick={() => dispatch({ type: "remove", value: id })}>
-                  //         x
-                  //       </Button>
-                  //     </CardBody>
-                  //   </Card>
-                  // </Label>
-
-                  // <Label sm={3} key={id}>
-                  //   <div className="add_product_icon">
-                  //     <img src={URL.createObjectURL(image.File)} className="plus_icon" alt="" />
-                  //   </div>
-                  // </Label>
-                );
-              })}
-            {/* <Label for="product_image" sm={2}>
-              <div className="add_product_icon">
-                <img
-                  onClick={() => dispatch({ type: "remove", value: id })}
-                  height="100%"
-                  src="https://lh3.googleusercontent.com/proxy/_Aa-XCNpidMCVNDGlG442IMU-Bc-OopXJWolFOSwJVZ8woNXdm7-xOOcb4192Al6U_ctNs-ldE7dT_K95OC3moKr2vIeNjZqx4ZQPRaSjKmFH_97tlrL"
-                  className="plus_icon overflow-hidden"
-                  alt=""
-                />
-              </div>
-            </Label> */}
+                ) : null
+              )}
             {addImage.length < 4 && (
               <Label for="product_image" sm={2}>
                 <div className="add_product_icon">
@@ -237,51 +235,6 @@ export function AddProduct() {
           </Col>
         </FormGroup>
       </Form>
-
-      {/* <FormGroup id="form-images" row>
-          <Label sm={2}>Image</Label>
-          <Col sm={10}>
-            <Label className="px-0 d-flex" sm={12}>
-              <div className="add_product_icon d-flex">
-                <div {...getRootProps()} className="m-auto">
-                  <input {...getInputProps()} />
-                  {!isDragActive && "Click here or drop a file to upload!"}
-                  {isDragActive && !isDragReject && "Drop it like it's hot!"}
-                  {isDragReject && "File type not accepted, sorry!"}
-                  {isFileTooLarge && <div className="text-danger mt-2">File is too large.</div>}
-                </div>
-              </div>
-            </Label>
-
-            <div className="mt-3 d-flex">
-              {addImage.length > 0 &&
-                addImage.map((acceptedFile, id) => {
-                  return (
-                    <Card className="add_product_card">
-                      <span
-                        onClick={() => dispatch({ type: "remove", value: id })}
-                        className="float-right"
-                        style={{ cursor: "pointer" }}>
-                        x
-                      </span>
-                      <CardImg top width="100%" src={URL.createObjectURL(acceptedFile.File)} alt="product" />
-                      <CardBody>
-                        <CardText>{acceptedFile.FileName}</CardText>
-                      </CardBody>
-                    </Card>
-                  );
-                })}
-            </div>
-            <CustomInput
-              sm={0}
-              className="add_product_input"
-              type="file"
-              id="product_image"
-              name="product_image"
-              label="select an image!"
-            />
-          </Col>
-        </FormGroup> */}
 
       <div className="form-group d-flex">
         <div className="mx-auto">
@@ -296,68 +249,3 @@ export function AddProduct() {
     </Fragment>
   );
 }
-
-// const AddImageDropzone = () => {
-//   const Layout = ({ input, previews, submitButton, dropzoneProps, files, extra: { maxFiles } }) => {
-//     return (
-//       <div>
-//         {/* <div {...dropzoneProps}>{files.length < maxFiles && input}</div> */}
-//         <div {...dropzoneProps}>
-//           {/* {input} */}
-//           {previews}
-//           {files.length < maxFiles && input}
-//           {/* {files.length > 0 && submitButton} */}
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   const getUploadParams = ({ meta }) => {
-//     const url = API_URL;
-//     return { url, meta: { fileUrl: `${URL.createObjectURL(meta.name)}` } };
-//   };
-
-//   const handleChangeStatus = ({ meta, remove }, status) => {
-//     console.log(status, meta);
-//     if (status === "header_received") remove();
-//   };
-
-//   const handleSubmit = (files, allFiles) => {
-//     console.log(files.map(File => File.meta));
-//     allFiles.forEach(File => dispatch({ type: "add", value: { FileName: File.name, File } }));
-//     // allFiles.forEach(f => f.remove())
-//   };
-
-//   console.log(addImage);
-
-//   return (
-//     <Dropzone
-//       // getUploadParams={getUploadParams}
-//       onChangeStatus={handleChangeStatus}
-//       addClassNames={{ dropzone: "add_product_icon" }}
-//       classNames={{ preview: "add_product_preview" }}
-//       onSubmit={handleSubmit}
-//       maxFiles={4}
-//       accept="image/png, image/jpeg"
-//       inputContent={(files, extra) => (extra.reject ? "Image files only" : "Drag Files")}
-//       styles={{
-//         dropzoneReject: { borderColor: "red", backgroundColor: "#DAA" },
-//         inputLabel: (files, extra) => (extra.reject ? { color: "red" } : {})
-//       }}
-//     />
-
-//     // <Dropzone
-//     //   getUploadParams={getUploadParams}
-//     //   onChangeStatus={handleChangeStatus}
-//     //   classNames={{ inputLabel: "add_product_input", dropzone: "add_product_icon", preview: "add_product_preview" }}
-//     //   LayoutComponent={Layout}
-//     //   onSubmit={handleSubmit}
-//     //   accept="image/png, image/jpeg"
-//     //   inputContent={(files, extra) => (extra.reject ? "Image files only" : "Drag Files")}
-//     //   styles={{
-//     //     dropzoneReject: { borderColor: "red", backgroundColor: "#DAA" },
-//     //     inputLabel: (files, extra) => (extra.reject ? { color: "red" } : {})
-//     //   }}
-//     // />
-//   );
-// };
