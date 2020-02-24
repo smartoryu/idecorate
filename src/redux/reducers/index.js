a9c6ee75c30135665e5dfaf2f7f09983c692b6e0
smartoryu <prikenang@gmail.com>
2020-02-24 17:23:49 +0700

    restructurized

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
 import AuthReducers from "./AuthReducers";
-import HandleReducers from "./HandleReducers";
-import FileReducers from "./FileReducers";
 import ProductReducers from "./ProductReducers";
 
 export default combineReducers({
   auth: AuthReducers,
-  handle: HandleReducers,
-  file: FileReducers,
   product: ProductReducers
 });
