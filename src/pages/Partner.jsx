98df554ed131a686540014f7e07bd5dd680f4fff
smartoryu <prikenang@gmail.com>
2020-02-27 16:37:47 +0700

    move fetchStore from profile page

--- src/pages/Partner.jsx
+++ src/pages/Partner.jsx
@@ -1,18 +1,34 @@
 import React, { Fragment, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, Route } from "react-router-dom";
 
 import { Product } from "../components/Partner/_Product";
 import { AddProduct } from "../components/Partner/_ProductAdd";
 import { Profile } from "../components/Partner/_Profile";
 import { Review } from "../components/Partner/_Review";
+import Axios from "axios";
+import { API_URL } from "../support/API_URL";
+import { STORE_GET } from "../support/types";
 
 function Partner({ match }) {
   const dispatch = useDispatch();
+  const userId = useSelector(state => state.partner.id);
 
   useEffect(() => {
     dispatch({ type: "RESET_PRODUCT" });
   });
+
+  useEffect(() => {
+    const fetchStore = async () => {
+      try {
+        let { data } = await Axios.get(`${API_URL}/partner?userid=${userId}`);
+        dispatch({ type: STORE_GET, payload: data.result });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchStore();
+  }, [userId, dispatch]);
 
   const PartnerSideMenu = () => {
     return (
@@ -42,13 +58,13 @@ function Partner({ match }) {
 
   return (
     <div className="partner-wrapper container-fluid my-3">
-      <div className="row mx-5 mb-5">
+      <div className="row ml-5 mr-0 mb-5">
         {/* SIDE MENU */}
         <section className="col-md-2 partner-sidemenu">{PartnerSideMenu()}</section>
 
         {/* CONTENT */}
         <div className="col-md-10 partner-content">
-          <div className="ml-5">
+          <div className="ml-0">
             <Route path={`${match.url}/`} component={Profile} exact />
             <Route path={`${match.url}/product`} component={Product} />
             <Route path={`${match.url}/add_product`} component={AddProduct} />
